c607daf50bc1878ee1337c24cc6c2413be24faa3
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-02-24 17:21:30 -0500

    Created Popular Restaurant Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 import PromotionBanner from "./components/4 Promotion Banner/PromotionBanner";
+import PopularRestaurant from "./components/6 Popular Restaurant/PopularRestaurant";
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent />
       <PromotionBanner />
+      <PopularRestaurant />
     </div>
   );
 }
