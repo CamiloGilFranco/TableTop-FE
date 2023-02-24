5da429eb01abb1f13c7a71259ea053ea235677c9
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-02-24 16:33:44 -0500

    created the info flow component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 
+import FlowInfoComponent from "./components/FlowInfoComponent/FlowInfoComponent";
+
 function App() {
   return (
     <div className="App">
       <HeaderComponent />
+
+      <FlowInfoComponent/>
     </div>
   );
 }
