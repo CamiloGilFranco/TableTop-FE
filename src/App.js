8a4c426d62a975ab4dd04133707e4c7e8892af65
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-02-24 12:00:00 -0500

    creates the SearchbarComponent and the assets folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
+import SearchbarComponent from "./components/SearchbarComponent/SearchbarComponent";
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent />
+      <SearchbarComponent />
     </div>
   );
 }
