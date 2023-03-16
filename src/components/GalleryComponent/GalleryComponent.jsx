84ddb5a84bddb5fe9af07fb31ba73cdf3abfaa8b
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-03-16 18:57:33 -0500

    Creacion de galeria con fotos random

--- src/components/GalleryComponent/GalleryComponent.jsx
+++ src/components/GalleryComponent/GalleryComponent.jsx
@@ -1,6 +1,26 @@
 import "./GalleryComponent.css";
-const GalleryComponent = ({ hiddenGallery }) => {
-  return <div className={`gallery-component-container ${hiddenGallery}`}></div>;
+
+const GalleryComponent = ({ photos, setCarousel, setPictureNumber }) => {
+  console.log(photos);
+  return (
+    <div className="gallery-component-container">
+      {photos.map((element, index) => {
+        return (
+          <img
+            src={element}
+            alt=""
+            key={index}
+            id={index}
+            className="gallery-component-prev-photo"
+            onClick={(event) => {
+              setCarousel(true);
+              setPictureNumber(index);
+            }}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default GalleryComponent;
