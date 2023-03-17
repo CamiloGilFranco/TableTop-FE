69bf6ffbe96e0b9cd812357a2338e0d167b40a5b
diegodag201611 <dgaleano201611@gmail.com>
2023-03-17 09:52:50 -0500

    add a new address component

--- src/components/payment/Payment.jsx
+++ src/components/payment/Payment.jsx
@@ -14,11 +14,11 @@ const Payment = () => {
                     <p className="payment-content__header--paragraph"><b>Debit Card</b></p>
                 </div>
                 <form action="" className="payment-form">
-                    <label for="nombre" className="payment-form-name">
+                    <label htmlFor="nombre" className="payment-form-name">
                         <p className="payment-form__paragraph">Name On Card</p>
                         <input  type="text" id="nombre" className="payment-form__input-name"/>
                     </label>
-                    <label for="number">
+                    <label htmlFor="number">
                         <p className="payment-card-paragraph">Card Number</p>
                         <div className="payment-card-info">
                             <input  type="number" id="number" className="payment-card-info__number"/>
@@ -26,36 +26,36 @@ const Payment = () => {
                         </div>
                     </label>
                 </form>
-                <form action="" className="form-card">
-                    <label for="month" className="form-card__date" >
+                <form action="" className="payment__form-card">
+                    <label htmlFor="month" className="form-card__date" >
                         <p className="card-paragraph">Month</p>
-                        <input  type="month" id="month" placeholder="Month..." className="form-card__imput"/>
+                        <input  type="month" id="month" placeholder="Month..." className="form-card__input"/>
                     </label>
-                    <label for="year" className="form-card__date">
+                    <label htmlFor="year" className="form-card__date">
                         <p className="card-paragraph">Year</p>
-                        <input  type="year" id="year" className="form-card__imput"/>
+                        <input  type="year" id="year" className="form-card__input"/>
                     </label>
-                    <label for="cvv" className="form-card__date">
+                    <label htmlFor="cvv" className="form-card__date">
                         <p className="card-paragraph">Cvv</p>
-                        <input  type="number" id="cvv" className="form-card__imput"/>
+                        <input  type="number" id="cvv" className="form-card__input"/>
                     </label>
                 </form>
                 <div className="button-payment">
-                    <button className="payment">MAKE PAYMENT</button>
+                    <button className="button-payment__make-pay">MAKE PAYMENT</button>
                 </div>
             </section>
-            <form className="type">
+            <form className="payment-form-way-pay">
                 <div className="type__payment">
                     <input type="radio" id="card" name="fav_language" />
-                    <label for="card" className="type-payment-text"><b>Credit Card</b></label><br/>
+                    <label htmlFor="card" className="type-payment-text"><b>Credit Card</b></label><br/>
                 </div>
                 <div className="type__payment">
                     <input type="radio" id="net" name="fav_language"/>
-                    <label for="net" className="type-payment-text"><b>Net Banking</b></label><br/>
+                    <label htmlFor="net" className="type-payment-text"><b>Net Banking</b></label><br/>
                 </div>
                 <div className="type__payment">
                     <input type="radio" id="wallet" name="fav_language" className="input-final"/>
-                    <label for="wallet" className="type-payment-text"><b>My Wallet</b></label>
+                    <label htmlFor="wallet" className="type-payment-text"><b>My Wallet</b></label>
                 </div>
             </form>
         </main>
--- src/pages/CheckoutPageComponent/CheckoutPageComponent.jsx
+++ src/pages/CheckoutPageComponent/CheckoutPageComponent.jsx
@@ -1,35 +1,38 @@
-import AlwaysFirstComponent from "../../components/AlwaysFirstComponent/AlwaysFirstComponent"
-import CartItem from "../../components/CartItem/CartItem"
-import { DeliveryAddressComponent } from "../../components/DeliveryAddressComponent/DeliveryAddressComponent" 
-import HeaderComponent from "../../components/HeaderComponent/HeaderComponent"
-import Footer from "../../components/Footer/Footer"
-import image from "./assets/pexels-ella-olsson-1640777.jpg"
-import {Payment} from "../../components/payment/Payment"
+import AlwaysFirstComponent from "../../components/AlwaysFirstComponent/AlwaysFirstComponent";
+import CartItem from "../../components/CartItem/CartItem";
+import { DeliveryAddressComponent } from "../../components/DeliveryAddressComponent/DeliveryAddressComponent";
+import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
+import Footer from "../../components/Footer/Footer";
+import image from "./assets/pexels-ella-olsson-1640777.jpg";
+import { Payment } from "../../components/payment/Payment";
+import "./CheckoutPageComponent.css";
 
 const CheckoutPageComponent = () => {
-    return (
-        <>
-            <div>
-                <div>
-                  <HeaderComponent />
-                </div>
-                <div>
-                    <img src={image} alt="" />
-                </div>
-                <div>
-                  <CartItem />
-                  <AlwaysFirstComponent />
-                </div>
-                <div>
-                  <DeliveryAddressComponent />
-                  <Payment />
-                </div>
-                <div>
-                  <Footer />
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <div>
+          <HeaderComponent />
+        </div>
+        <div>
+          <img src={image} alt="" className="imageHeader"/>
+        </div>
+        <div className="checkout-container">
+          <div>
+            <CartItem />
+            <AlwaysFirstComponent />
+          </div>
+          <div>
+            <DeliveryAddressComponent />
+            <Payment />
+          </div>
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export {CheckoutPageComponent}
+export { CheckoutPageComponent };
