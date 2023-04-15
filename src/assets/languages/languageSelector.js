8b3fc96815a9fa29dc4a6732f39d2b503c9ffcfd
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-04-15 14:34:39 -0500

    adds the conditionally rendered buttons to the log in modal

--- src/assets/languages/languageSelector.js
+++ src/assets/languages/languageSelector.js
@@ -233,7 +233,9 @@ const languageSelector = (lang, str) => {
     generalError: 'An error has ocurred, please try again.',
     logInSuccessfull: 'You have logged in successfully!',
     userUpdatedMessage: 'User updated successfully!',
-    userUpdateFailedMessage: "Couldn't update the user, there's been an error, please try again."
+    userUpdateFailedMessage: "Couldn't update the user, there's been an error, please try again.",
+    userPage: 'Go to the user page',
+    logOutSuccess: "You've logged out successfully",
   };
 
   const es = {
@@ -473,7 +475,9 @@ const languageSelector = (lang, str) => {
     generalError: 'Ocurrio un error, por favor vuelve a intentarlo.',
     logInSuccessfull: '¡Has iniciado sesión correctamente!',
     userUpdatedMessage: '¡Usuario actualizado con éxito!',
-    userUpdateFailedMessage: 'No se pudo actualizar el usuario, ocurrio un error, intenta nuevamente.'
+    userUpdateFailedMessage: 'No se pudo actualizar el usuario, ocurrio un error, intenta nuevamente.',
+    userPage: 'Ve a la pagina de usuario',
+    logOutSuccess: "Sesión terminada correctamente",
   };
 
   if (lang === "en") {
--- src/components/LogInComponent/LogInComponent.jsx
+++ src/components/LogInComponent/LogInComponent.jsx
@@ -1,48 +1,77 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Cookies from 'universal-cookie';
+import { toast, ToastContainer } from 'react-toastify';
 import ForgottenPasswordComponent from "./ForgottenPasswordComponent";
 import LoginFormComponent from "./LoginFormComponent";
 import "./LogInComponent.css";
 import languageSelector from '../../assets/languages/languageSelector';
+import { setUser } from '../../store/actions/user.action';
 
 const LogInComponent = ({ setShowLogIn }) => {
 
   const language = useSelector(state=> state.languageReducer);
-
+  const user = useSelector(state => state.userReducer.user);
+  const dispatch = useDispatch();
   const [whichForm, setWhichForm] = useState(true);
   const navigate = useNavigate();
+  const cookies = new Cookies();
+
+  const handleLogout = () => {
+    dispatch(setUser({}));
+    cookies.remove("token");
+    cookies.remove("name");
+    cookies.remove("last_name");
+    cookies.remove("email");
+    toast.success(languageSelector(language, "logOutSuccess"));
+  };
+  const isUserLoggedIn = Object.keys(user).length;
 
   return (
-    <div className="log-in-component">
-      <div
-        className="log-in-component-background"
-        onClick={() => setShowLogIn(false)}
-      ></div>
-      <div className="log-in-form">
-        <div className="log-in-form-title-box">
-          <span className="log-in-main-title">{languageSelector(language, 'logInTitle')}</span>
-          <span className="log-in-main-subtitle">
-            {languageSelector(language, 'logInSubtitle')}
-          </span>
-        </div>
-        <div className="log-in-form-main-form">
-          {whichForm ? (
-            <LoginFormComponent setWhichForm={setWhichForm} closeModal={() => setShowLogIn(false)}/>
-          ) : (
-            <ForgottenPasswordComponent setWhichForm={setWhichForm} />
-          )}
-        </div>
-        <div className="log-in-sign-up">
-          <span
-            className="log-in-sign-up-text"
-            onClick={() => navigate("/register")}
-          >
-            {languageSelector(language, 'logInRegister')}
-          </span>
+    <>
+      <ToastContainer/>
+      <div className="log-in-component">
+        <div
+          className="log-in-component-background"
+          onClick={() => setShowLogIn(false)}
+        ></div>
+        <div className="log-in-form">
+          <div className="log-in-form-title-box">
+            <span className="log-in-main-title">{languageSelector(language, 'logInTitle')}</span>
+            <span className="log-in-main-subtitle">
+              {languageSelector(language, 'logInSubtitle')}
+            </span>
+          </div>
+          <div className="log-in-form-main-form">
+          {isUserLoggedIn ? (
+              <div className='log-in-main-form-buttons-container'>
+                <button className='log-in-main-form-buttons' onClick={() => navigate('/user')}>
+                  {languageSelector(language, 'userPage')}
+                </button>
+                <button className='log-in-main-form-buttons' onClick={handleLogout}>
+                  {languageSelector(language, 'logOutText')}
+                </button>
+              </div>
+            ) : (
+              whichForm ? (
+                <LoginFormComponent setWhichForm={setWhichForm} closeModal={() => setShowLogIn(false)}/>
+              ) : (
+                <ForgottenPasswordComponent setWhichForm={setWhichForm} />
+              )
+            )}
+          </div>
+          <div className="log-in-sign-up">
+            <span
+              className="log-in-sign-up-text"
+              onClick={() => navigate("/register")}
+            >
+              {languageSelector(language, 'logInRegister')}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
