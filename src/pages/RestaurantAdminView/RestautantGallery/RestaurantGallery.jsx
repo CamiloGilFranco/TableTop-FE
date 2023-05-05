e98c6c0c1e4b972872a3ebefcc3ae839be97087d
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-05-04 21:10:04 -0500

    adds deletion functionlity to the restaurant photos

--- src/pages/RestaurantAdminView/RestautantGallery/RestaurantGallery.jsx
+++ src/pages/RestaurantAdminView/RestautantGallery/RestaurantGallery.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./RestaurantGallery.css";
 import GalleryComponent from "../../../components/GalleryComponent/GalleryCarouselComponent";
-import ControlledCarousel from "../../../components/GalleryComponent/GalleryComponent";
 import { AiOutlineFileAdd, AiFillDelete } from "react-icons/ai";
 import Cookies from "universal-cookie";
 import axios from "axios";
@@ -14,7 +13,6 @@ const RestaurantGallery = ({
   restaurant = {},
   onPhotoUpdate,
 }) => {
-  const [carousel, setCarousel] = useState(false);
   const [pictureNumber, setPictureNumber] = useState(0);
   const [adding, setAdding] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
@@ -27,7 +25,6 @@ const RestaurantGallery = ({
   };
 
   const { photos } = restaurant;
-
   const handleAddPhoto = async (event) => {
     event.preventDefault();
 
@@ -58,8 +55,34 @@ const RestaurantGallery = ({
     }
   };
 
-  const handleDeletePhoto = () => {
-    console.log("Deleting photo...");
+  const handleDeletePhoto = async () => {
+    const photoToDelete = photos[pictureNumber];
+
+    if (
+      photoToDelete &&
+      window.confirm(languageSelector(language, "deletePhotoWarning"))
+    ) {
+      try {
+        const response = await axios.delete(
+          `${API_URL}/photos-restaurant/${photoToDelete.id_restaurant_photo}`,
+          config
+        );
+        if (response.status === 200 || response.status === 204) {
+          toast.success(
+            languageSelector(language, "restaurantPhotoDeleteSuccess")
+          );
+          onPhotoUpdate();
+          setAdding(false);
+          onPhotoUpdate();
+        } else {
+          toast.error(
+            languageSelector(language, "restaurantPhotoDeleteFailure")
+          );
+        }
+      } catch (error) {
+        toast.error(languageSelector(language, "restaurantPhotoDeleteFailure"));
+      }
+    }
   };
 
   const handleFileInputChange = (event) => {
@@ -67,48 +90,48 @@ const RestaurantGallery = ({
   };
 
   return (
-    <div className="restaurant-gallery">
-      <div className="gallery-header">
-        <h2>{languageSelector(language, "restaurantGalleryTitle")}</h2>
-        <div className="gallery-header-icons">
-          <button onClick={() => setAdding(true)}>
-            <AiOutlineFileAdd />
-          </button>
-          <button onClick={handleDeletePhoto}>
-            <AiFillDelete />
-          </button>
+    <>
+      <div className="restaurant-gallery">
+        <div className="gallery-header">
+          <h2>{languageSelector(language, "restaurantGalleryTitle")}</h2>
+          <div className="gallery-header-icons">
+            <button onClick={() => setAdding(true)}>
+              <AiOutlineFileAdd />
+            </button>
+            <button onClick={handleDeletePhoto}>
+              <AiFillDelete />
+            </button>
+          </div>
         </div>
+        <p>{languageSelector(language, 'restaurantPhotosText')}</p>
+        {adding ? (
+          <div className="gallery-form">
+            <form>
+              <label htmlFor="photo">
+                {languageSelector(language, "restaurantGalleryAddPhoto")}:
+              </label>
+              <input
+                type="file"
+                name="photo"
+                id="photo"
+                onChange={handleFileInputChange}
+              />
+              <button type="submit" onClick={handleAddPhoto}>
+                {languageSelector(language, "restaurantGalleryAddButton")}
+              </button>
+              <button type="button" onClick={() => setAdding(false)}>
+                {languageSelector(language, "restaurantGalleryAddCancel")}
+              </button>
+            </form>
+          </div>
+        ) : (
+          <GalleryComponent
+            photos={photos}
+            setPictureNumber={setPictureNumber}
+          />
+        )}
       </div>
-      {adding ? (
-        <div className="gallery-form">
-          <form>
-            <label htmlFor="photo">
-              {languageSelector(language, "restaurantGalleryAddPhoto")}:
-            </label>
-            <input
-              type="file"
-              name="photo"
-              id="photo"
-              onChange={handleFileInputChange}
-            />
-            <button type="submit" onClick={handleAddPhoto}>
-              {languageSelector(language, "restaurantGalleryAddButton")}
-            </button>
-            <button type="button" onClick={() => setAdding(false)}>
-              {languageSelector(language, "restaurantGalleryCancelButton")}
-            </button>
-          </form>
-        </div>
-      ) : carousel ? (
-        <ControlledCarousel photos={photos} pictureNumber={pictureNumber} />
-      ) : (
-        <GalleryComponent
-          photos={photos}
-          setCarousel={setCarousel}
-          setPictureNumber={setPictureNumber}
-        />
-      )}
-    </div>
+    </>
   );
 };
 
